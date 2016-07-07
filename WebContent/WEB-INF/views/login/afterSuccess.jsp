67ee0f517b989257052a583d5bc43ed7b1e2d2e9
chenivan0214 <chenjiahau@snailhouse.com.tw>
2016-07-07 08:23:32 +0800

    Change

--- WebContent/WEB-INF/views/login/afterSuccess.jsp
+++ WebContent/WEB-INF/views/login/afterSuccess.jsp
@@ -1,5 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ page import="com.opensymphony.xwork2.util.*" %>
 <%@ taglib prefix="s" uri="/struts-tags" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -9,19 +8,6 @@
 </head>
 <body>
 
-loginlogin
-<br/>
-${sessionScope.type}
-<br/>
-<%
-//import com.opensymphony.xwork2.util.*
-ValueStack valueStack = (ValueStack)request.getAttribute("struts.valueStack");
-String type = (String)valueStack.findValue("type");
-%>
-<%=type %>
-<br/>
-<s:property value="%{type}" />
-or
 <s:property value="type" />
 
 </body>
--- src/main/java/com/action/login/IndexAction.java
+++ src/main/java/com/action/login/IndexAction.java
@@ -4,19 +4,38 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.ServletActionContext;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class IndexAction extends ActionSupport {
     private static final long serialVersionUID = 1L;
+    
+    private String type;
     
     //@Override
     //public String execute() throws Exception {
     //    return ActionSupport.SUCCESS;
     //}
     
+    public void setType(String _type) {
+        type = _type;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
     public String index() {
-        HttpServletRequest req = ServletActionContext.getRequest();
-        System.out.println(req.getParameter("type"));
+        System.out.println(getType());
+        //HttpServletRequest req = ServletActionContext.getRequest();
+        //String type = req.getParameter("type");
+        //System.out.println(req.getParameter("type"));
+        ActionContext.getContext().getSession().put("type", getType());
+        
         return "index_success";
+    }
+    
+    public String after() {
+        return "after_success";
     }
 }
